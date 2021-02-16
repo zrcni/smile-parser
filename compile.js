ab114fd3cebf07d65d49b5f6a8bf8cf37f395761
zrcni <zrcni9@gmail.com>
2021-02-16 09:14:06 +0200

    updated minified emojis to be backwards compatible

--- compile.js
+++ compile.js
@@ -1,22 +1,30 @@
-const fs = require("fs");
+const fs = require("fs")
 
 fs.readFile("./emojidata/emoji.json", "utf8", (err, data) => {
-  if (err) throw err;
-  const emojiArray = [];
-  const emojis = JSON.parse(data);
+  if (err) throw err
+  const emojiArray = []
+  const emojis = JSON.parse(data)
   for (const emoji of emojis) {
     const newEmoji = {
-      image: emoji.image
-    };
-    if( emoji.texts) {
+      short_name: emoji.short_name,
+      image: emoji.image,
+    }
+    if (emoji.text) {
+      newEmoji.text = emoji.text
+    }
+    if (emoji.texts) {
       newEmoji.texts = emoji.texts
     }
     if (emoji.short_names) {
       newEmoji.short_names = emoji.short_names
     }
-    emojiArray.push(newEmoji);
+    emojiArray.push(newEmoji)
   }
-  fs.writeFileSync(`./src/minifiedEmoji.json`, JSON.stringify(emojiArray), err => {
-    if (err) throw err;
-  });
-});
+  fs.writeFileSync(
+    `./src/minifiedEmoji.json`,
+    JSON.stringify(emojiArray),
+    (err) => {
+      if (err) throw err
+    }
+  )
+})
